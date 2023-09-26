b813239ad65cd1c460fe083036e5d65b19be8080
hazeluff <eugene@hazeluff.com>
2023-09-25 22:37:34 -0700

    Update GDC help message

--- src/main/java/com/hazeluff/discord/bot/gdc/GameDayChannel.java
+++ src/main/java/com/hazeluff/discord/bot/gdc/GameDayChannel.java
@@ -788,10 +788,9 @@ public class GameDayChannel extends Thread implements IEventProcessor {
 	}
 
 	private Message sendGDCHelpMessage(TextChannel channel) {
-		String strMessage = String.join("\n", 
-				buildDetailsMessage(),
-				getHelpMessageText()
-		);
+		String strMessage = buildDetailsMessage()
+				+ "\n\n"
+				+ getHelpMessageText();
 		Message message = DiscordManager.sendAndGetMessage(channel,
 				MessageCreateSpec.builder()
 					.content(strMessage)
@@ -802,7 +801,7 @@ public class GameDayChannel extends Thread implements IEventProcessor {
 	}
 
 	private String getHelpMessageText() {
-		return "**This game/channel is interactable with Slash Commands!**"
+		return "This game/channel is interactable with Slash Commands!"
 				+ "\nUse `/gdc subcommand:help` to bring up a list of commands.";
 	}
 

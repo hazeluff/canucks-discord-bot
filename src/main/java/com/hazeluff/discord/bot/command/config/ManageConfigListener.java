4622f1a0a935a9e7606b31cb819e23565584d5fd
Eugene <eugene@hazeluff.com>
2026-03-18 16:50:02 -0700

    Update ManageConfigListener.java

--- src/main/java/com/hazeluff/discord/bot/command/config/ManageConfigListener.java
+++ src/main/java/com/hazeluff/discord/bot/command/config/ManageConfigListener.java
@@ -1,7 +1,10 @@
 package com.hazeluff.discord.bot.command.config;
 
+import java.util.function.Supplier;
+
 import com.hazeluff.discord.bot.NHLBot;
 import com.hazeluff.discord.bot.command.ConfigCommand;
+import com.hazeluff.discord.bot.command.InteractionUtils;
 import com.hazeluff.discord.bot.database.preferences.GuildPreferences;
 import com.hazeluff.discord.bot.discord.DiscordManager;
 import com.hazeluff.discord.bot.gdc.nhl.NHLGameDayWatchChannel;
@@ -10,6 +13,7 @@ import com.hazeluff.discord.bot.listener.EventListener;
 import discord4j.core.event.domain.Event;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.spec.InteractionReplyEditSpec;
 
 public class ManageConfigListener extends EventListener {
 	public ManageConfigListener(NHLBot nhlBot) {
@@ -24,6 +28,18 @@ public class ManageConfigListener extends EventListener {
 	}
 
 	public void processEvent(ButtonInteractionEvent event) {
+
+		
+		replyAndDeferEdit(event,
+			"Processing your config change...",
+			() -> processCommand(event),
+			() -> {
+				return buildReplyEdit(event);
+			}
+		);
+	}
+
+	private void processCommand(ButtonInteractionEvent event) {
 		Guild guild = DiscordManager.block(event.getInteraction().getGuild());
 		switch(event.getCustomId()) {
 		case ConfigCommand.SINGLE_BUTTON_ID:
@@ -42,15 +58,39 @@ public class ManageConfigListener extends EventListener {
 		if(pref.isUseChannelThreads() == useThread)
 			return;
 		
-		// Update preferences
+		// Update preferences; save to DB
 		pref.setUseChannelThreads(useThread);
 		nhlBot.getPersistentData().getPreferencesData().savePreferences(guildId, pref);
 
-		NHLGameDayWatchChannel gdwChannel = NHLGameDayWatchChannel.getChannel(guildId);
-		if (gdwChannel == null)
-			gdwChannel = NHLGameDayWatchChannel.getOrCreate(nhlBot, guild);
-		else
-			gdwChannel.changeThreadUsage(useThread);
+		// Update the GameDayWatchChannel
+		if (!pref.getTeams().isEmpty()) {
+			NHLGameDayWatchChannel gdwChannel = NHLGameDayWatchChannel.getChannel(guildId);
+			if (gdwChannel == null)
+				gdwChannel = NHLGameDayWatchChannel.getOrCreate(nhlBot, guild);
+			else
+				gdwChannel.changeThreadUsage(useThread);
+		}
+	}
+	
+	InteractionReplyEditSpec buildReplyEdit(ButtonInteractionEvent event) {
+		String message = "Done";
+		switch (event.getCustomId()) {
+		case ConfigCommand.SINGLE_BUTTON_ID:
+			message = "Games now post directly to #game-day-watch";
+			break;
+		case ConfigCommand.THREAD_BUTTON_ID:
+			message = "Games now post to threads within #game-day-watch";
+			break;
+		}
+		return InteractionReplyEditSpec.builder().contentOrNull(message).build();
+	}
 
+	private void replyAndDeferEdit(
+		ButtonInteractionEvent event,
+		String initialReply,
+		Runnable defferedAction,
+		Supplier<InteractionReplyEditSpec> defferedReplySupplier) {
+		DiscordManager
+			.subscribe(InteractionUtils.replyAndDeferEdit(event, initialReply, defferedAction, defferedReplySupplier));
 	}
 }
